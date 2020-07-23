38c58e1c75af476ab6813d01e93f84f366346fb1
sAVItar02 <59026666+sAVItar02@users.noreply.github.com>
2020-07-23 16:56:10 +0530

    Enemy movement path (0)

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    //Config Params
+    [SerializeField] List<Transform> Waypoints;
+    [SerializeField] float moveSpeed = 2f;
+
+    int waypointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = Waypoints[waypointIndex].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyMove();
+
+    }
+
+    private void EnemyMove()
+    {
+        if (waypointIndex <= Waypoints.Count - 1)
+        {
+            var targetPosition = Waypoints[waypointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
